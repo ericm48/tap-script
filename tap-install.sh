dc4d81519db3b68fb6b7c64f4dc85535bb012864
ericm48 <me_manley@yahoo.com>
2023-07-26 14:36:35 -0600

    -Incremental Commit.

--- tap-install.sh
+++ tap-install.sh
@@ -409,6 +409,6 @@ fi
          export INSTALL_REGISTRY_PASSWORD=$tanzunetpassword
          export INSTALL_REGISTRY_HOSTNAME=registry.tanzu.vmware.com
          tanzu secret registry add tap-registry --username ${INSTALL_REGISTRY_USERNAME} --password ${INSTALL_REGISTRY_PASSWORD} --server ${INSTALL_REGISTRY_HOSTNAME} --export-to-all-namespaces --yes --namespace tap-install
-         echo "#####################################################################################################"
-         echo "########### Rebooting #############"
-         sudo reboot
+         #echo "#####################################################################################################"
+         #echo "########### Rebooting #############"
+         #sudo reboot
